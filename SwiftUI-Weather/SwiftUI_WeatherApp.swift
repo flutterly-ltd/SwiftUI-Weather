5a001b1e71f60c232d08cc6858d51c85ae9b1a8c
Anoop Jose <anoop@flutterly.co.uk>
2021-09-04 10:10:41 +0100

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Anoop Jose on 04/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
